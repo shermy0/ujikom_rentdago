b3c4eee47a7743a0ecf20ce73a7c8114ab45ea2b
starfollowme <irhammuhamad124@gmail.com>
2026-04-09 10:02:30 +0700

    feat: add frontend order history page to display and manage user bookings

--- resources/views/frontend/order/index.blade.php
+++ resources/views/frontend/order/index.blade.php
@@ -12,7 +12,7 @@
 
     {{-- HEADER DETAIL PRODUK --}}
     <div class="product-detail-header">
-        <a href="{{ route('profile.index') }}" class="header-back">
+        <a href="{{ route('home') }}" class="header-back">
             <i class="fa fa-arrow-left"></i>
         </a>
         <div class="header-title">Pesanan Saya</div>
